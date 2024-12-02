591c6a3980fe725988e04b82afd48432a2e6fbd7
mikeythacker <mikeythacker@icloud.com>
2024-12-02 14:40:57 +0000

    Day 1

--- Day 1: Historian Hysteria.py
+++ Day 1: Historian Hysteria.py
@@ -1,5 +1,26 @@
+def get_lists():
+    list1 = []
+    list2 = []
+
+    with open("Day1Nums.txt", "r") as f:
+        while True:
+            current_nums = f.readline()
+            if current_nums == "":
+                break
+
+            list1.append(int(current_nums[0:5]))
+            list2.append(int(current_nums[8:]))
+    return sorted(list1), sorted(list2)
+
+
 def main():
-    return None
+    list1, list2 = get_lists()
+
+    total_diff = 0
+    for i in range(len(list1)):
+        total_diff += abs(list1[i] - list2[i])
+
+    print(total_diff)
 
 
 main()
